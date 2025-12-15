9ecb45b06af64ace77edb8f35b881bedf99082ac
Rajagarwal632 <raj390504@gmail.com>
2025-12-16 00:29:39 +0530

    /user/signup defined

--- route/user.js
+++ route/user.js
@@ -9,8 +9,34 @@ const JWT_USER = process.env.JWT_USER
 
 const {usermodel} = require("../config/db")
 
-userroute.post("signup" , async function(req,res) {
-    
+userroute.post("/signup" , async function(req,res) {
+    const reqbody = z.object({
+        email : z.string().email(),
+        password : z.string().min(3),
+        name : z.string()
+    })
+    const parsedatawithsucess = reqbody.safeParse(req.body)
+    if(!parsedatawithsucess.success){
+        res.json({
+            msg : "INCORRECT FORMAT",
+            error : parsedatawithsucess.error
+        })
+        return
+    }
+    const email = req.body.email
+    const password = req.body.password
+    const name = req.body.name
+
+    const hashpassword = await bcrypt.hash(password,saltround)
+
+    await usermodel.create({
+        email,
+        password : hashpassword,
+        name
+    })
+    res.json({
+        msg : "USER CREATED"
+    })
 })
 
 userroute.post("/signin" , async function(req,res){
