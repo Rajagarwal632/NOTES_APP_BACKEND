43aabd8ec812812bc0436a274cb30c552b2f9c6a
Rajagarwal632 <raj390504@gmail.com>
2025-12-16 01:17:06 +0530

    /note/create defined

--- route/note.js
+++ route/note.js
@@ -3,9 +3,33 @@ const noteroute = Router()
 const {z} = require("zod")
 
 const mongoose = require("mongoose")
+const {notesmodel } = require("../config/db")
+const {userauth} = require("../middleware/userauth")
 
 noteroute.post("/",userauth , async function(req,res){
-    
+    const reqbody = z.object({
+        title : z.string(),
+        content : z.string()
+    })
+    const parsedatawithsucess = reqbody.safeParse(req.body)
+    if(!parsedatawithsucess.success){
+        res.json({
+            msg : "INCORRECT FORMAT",
+            error : parsedatawithsucess.error
+        })
+        return
+    }
+    const title = req.body.title
+    const content = req.body.content
+    const userid = req.userid
+    await notesmodel.create({
+        title,
+        content,
+        userid
+    })
+    res.json({
+        msg : "NOTE CREATED"
+    })
 })
 noteroute.put("/:id",userauth , async function(req,res){
 
