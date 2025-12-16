a78631afc5e811e2c27c04d961a44182767af2f3
Rajagarwal632 <raj390504@gmail.com>
2025-12-16 11:32:49 +0530

    /note/update any note EP defined

--- route/note.js
+++ route/note.js
@@ -32,7 +32,37 @@ noteroute.post("/",userauth , async function(req,res){
     })
 })
 noteroute.put("/:id",userauth , async function(req,res){
-
+    const userid = req.userid
+    const note_id = req.params.id
+    const update = z.object({
+        title : z.string().optional(),
+        content : z.string().optional()
+    })
+    const parsedatawithsucess = update.safeParse(req.body)
+    if(!parsedatawithsucess.success){
+        res.json({
+            msg : "INCORRECT FORMAT",
+            error : parsedatawithsucess.error
+        })
+    }
+    const title = req.body.title
+    const content= req.body.content
+    const update_note = await notesmodel.findOneAndUpdate({
+        userid : userid,
+        _id : note_id
+    },{
+        title,
+        content
+    })
+    if(update_note){
+        res.json({
+            msg : "NOTE UPDATED PLEASE CHECK"
+        })
+    }else{
+        res.json({
+            msg : "EITHER USER_ID OR NOTE_ID NOT MATCHED"
+        })
+    }
 })
 noteroute.delete("/:id",userauth , async function(req,res){
 
